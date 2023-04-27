dff11bfcda75feede906882d44aaaceb3ec0ec8c
Wilbur42 <Will007ward@gmail.com>
2023-04-27 21:38:08 +0100

    Initial Commit

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# Screen-Lock
+
+## Disclaimer
+This tool is for educational purposes only. NOT to be used without persmission from the device/acount owner.
+
+I am not responsible for any damage you cause with this tool. Use it at your own risk.
+
+## Description
+This tool is used to lock the screen of a device with an image of choice, as well as locking keyboard and mouse inputs.
---
+++ main.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import tkinter
+import threading
+from time import sleep
+
+import keyboard
+from pynput.mouse import Controller
+from PIL import Image, ImageTk # pip install pillow
+
+
+def blockinput():
+    global block_input_flag
+    block_input_flag = True
+    t1 = threading.Thread(target=blockinput_start)
+    t1.start()
+    print('[SUCCESS] Input blocked!')
+
+def unblockinput():
+    blockinput_stop()
+    print('[SUCCESS] Input unblocked!')
+
+def blockinput_start():
+    global block_input_flag
+    mouse = Controller()
+    for i in range(150):
+        keyboard.block_key(i)
+    while block_input_flag:
+        mouse.position = (0, 0)
+
+def blockinput_stop():
+    global block_input_flag
+    for i in range(150):
+        keyboard.unblock_key(i)
+    block_input_flag = False
+
+def MainLoop(tk):
+    sleep(5) # Will Lock for 5 seconds
+    unblockinput()
+    tk.quit()
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath('.')
+
+    return os.path.join(base_path, relative_path)
+
+if __name__ == '__main__':
+
+    block_input_flag = False
+
+    blockinput()
+
+    tk = tkinter.Tk()
+
+    tk.attributes('-fullscreen', True)
+    tk.attributes('-topmost', True)
+
+    width = tk.winfo_screenwidth()
+    height = tk.winfo_screenheight()
+
+    img = Image.open(resource_path('background.jpg')).resize((width, height))
+    img = ImageTk.PhotoImage(img)
+
+    tkinter.Label(image=img).pack()
+
+    t = threading.Thread(target=MainLoop, args=(tk,))
+    t.start()
+
+    tk.protocol('WM_DELETE_WINDOW', lambda: sys.exit())
+    tk.mainloop()
