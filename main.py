c8a2ae9715ae6f5d30362ef578791bf6fbd272bc
Wilbur42 <Will007ward@gmail.com>
2023-05-18 22:25:17 +0100

    Encapsulated in Class

--- main.py
+++ main.py
@@ -8,71 +8,92 @@ from time import sleep
 
 import keyboard
 from pynput.mouse import Controller
-from PIL import Image, ImageTk # pip install pillow
-import PyInstaller.__main__ # pip install pyinstaller
+from PIL import Image, ImageTk
+import PyInstaller.__main__
 
-block_input_flag = False
+class ScreenLock:
+    def __init__(self, duration, name, background):
+        self.duration = duration
+        self.name = name
+        self.background = background
+        self.block_input_flag = False
+        self.tk = None
 
-def Install(name, background):
-    PyInstaller.__main__.run([
-        os.path.abspath(__file__),
-        f'--name={name}',
-        '--onefile',
-        '--windowed',
-        '--add-data',
-        f'{background};.'
-    ])
+    def install(self):
+        PyInstaller.__main__.run([
+            os.path.abspath(__file__),
+            f'--name={self.name}',
+            '--onefile',
+            '--windowed',
+            '--add-data',
+            f'{self.background};.'
+        ])
 
-    shutil.move(os.getcwd() + f'\\dist\\{name}.exe', os.getcwd())
+        shutil.move(os.path.join(os.getcwd(), f'dist/{self.name}.exe'), os.getcwd())
 
-    os.remove(f'{name}.spec')
+        os.remove(f'{self.name}.spec')
 
-    sleep(5)
-    shutil.rmtree('./build/')
-    shutil.rmtree('./dist/')
+        sleep(5)
+        shutil.rmtree('./build/')
+        shutil.rmtree('./dist/')
 
-def blockinput():
-    global block_input_flag
-    block_input_flag = True
-    t1 = threading.Thread(target=blockinput_start)
-    t1.start()
-    print('[SUCCESS] Input blocked!')
+    def block_input(self):
+        self.block_input_flag = True
+        t1 = threading.Thread(target=self.block_input_start)
+        t1.start()
+        print('[SUCCESS] Input blocked!')
 
-def unblockinput():
-    blockinput_stop()
-    print('[SUCCESS] Input unblocked!')
+    def unblock_input(self):
+        self.block_input_stop()
+        print('[SUCCESS] Input unblocked!')
 
-def blockinput_start():
-    global block_input_flag
-    mouse = Controller()
-    for i in range(150):
-        keyboard.block_key(i)
-    while block_input_flag:
-        mouse.position = (0, 0)
+    def block_input_start(self):
+        mouse = Controller()
+        for i in range(150):
+            keyboard.block_key(i)
+        while self.block_input_flag:
+            mouse.position = (0, 0)
 
-def blockinput_stop():
-    global block_input_flag
-    for i in range(150):
-        keyboard.unblock_key(i)
-    block_input_flag = False
+    def block_input_stop(self):
+        for i in range(150):
+            keyboard.unblock_key(i)
+        self.block_input_flag = False
 
-def MainLoop(tk, duration):
-    sleep(duration)
-    unblockinput()
-    tk.quit()
+    def main_loop(self):
+        sleep(self.duration)
+        self.unblock_input()
+        self.tk.quit()
 
-def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except AttributeError:
-        base_path = os.path.abspath('.')
+    def resource_path(self, relative_path):
+        try:
+            base_path = sys._MEIPASS
+        except AttributeError:
+            base_path = os.path.abspath('.')
 
-    return os.path.join(base_path, relative_path)
+        return os.path.join(base_path, relative_path)
+
+    def run(self):
+        self.block_input()
+
+        self.tk = tkinter.Tk()
+        self.tk.attributes('-fullscreen', True)
+        self.tk.attributes('-topmost', True)
+
+        width = self.tk.winfo_screenwidth()
+        height = self.tk.winfo_screenheight()
+
+        img = Image.open(self.resource_path(self.background)).resize((width, height))
+        img = ImageTk.PhotoImage(img)
+
+        tkinter.Label(image=img).pack()
+
+        t = threading.Thread(target=self.main_loop)
+        t.start()
+
+        self.tk.protocol('WM_DELETE_WINDOW', lambda: sys.exit())
+        self.tk.mainloop()
 
 if __name__ == '__main__':
-
-    block_input_flag = False
-
     parser = argparse.ArgumentParser(description='Screen lock application')
     parser.add_argument('-d', '--duration', type=int, default=5, help='The duration in seconds')
     parser.add_argument('-n', '--name', default='ScreenLock', help='The name of the application')
@@ -82,51 +103,22 @@ if __name__ == '__main__':
     parser.add_argument('--uninstall', action='store_true', help='Uninstall the application')
     args = parser.parse_args()
 
-    duration = args.duration
-    name = args.name
-    background = args.background
-
-    if name:
-        if not name.endswith('.exe'):
-            name += '.exe'
-
-        if not os.path.exists(name):
-            print('File not found, installing...')
-            Install(name, background)
-            sys.exit()
+    screen_lock = ScreenLock(args.duration, args.name, args.background)
 
     if args.uninstall:
-        if os.path.exists(name):
-            os.remove(name)
+        if os.path.exists(screen_lock.name):
+            os.remove(screen_lock.name)
         sys.exit()
 
-    if args.install:
-        Install(name, background)
+    if args.install or (screen_lock.name and not os.path.exists(screen_lock.name)):
+        print('File not found, installing...')
+        screen_lock.install()
         sys.exit()
 
     if args.update:
-        if os.path.exists(name):
-            os.remove(name)
-        Install(name, background)
+        if os.path.exists(screen_lock.name):
+            os.remove(screen_lock.name)
+        screen_lock.install()
         sys.exit()
 
-    blockinput()
-
-    tk = tkinter.Tk()
-
-    tk.attributes('-fullscreen', True)
-    tk.attributes('-topmost', True)
-
-    width = tk.winfo_screenwidth()
-    height = tk.winfo_screenheight()
-
-    img = Image.open(resource_path(background)).resize((width, height))
-    img = ImageTk.PhotoImage(img)
-
-    tkinter.Label(image=img).pack()
-
-    t = threading.Thread(target=MainLoop, args=(tk, duration))
-    t.start()
-
-    tk.protocol('WM_DELETE_WINDOW', lambda: sys.exit())
-    tk.mainloop()
+    screen_lock.run()
